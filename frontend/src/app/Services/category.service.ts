e0251f920e297986d257c5abe1b70a20cc333247
xvimnt <jvmonteros98@gmail.com>
2020-04-27 14:59:10 -0600

    fixed Services

--- frontend/src/app/Services/category.service.ts
+++ frontend/src/app/Services/category.service.ts
@@ -1,10 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Router } from "@angular/router";
+import { map } from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router) { }
+ 
+  headers: HttpHeaders = new HttpHeaders({
+    "Content-Type": "application/json"
+  })
+
+  InsertUser(name: string, desc: string) {
+    const url = "http://localhost:3000/addCategory"
+    return this.http.post(
+      url,
+      {
+        name,
+        desc
+      },
+      { headers: this.headers }
+    ).pipe(map(data => data));
+  }
 }
--- frontend/src/app/Services/product.service.ts
+++ frontend/src/app/Services/product.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Router } from "@angular/router";
+import { map } from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router) { }
+ 
+  headers: HttpHeaders = new HttpHeaders({
+    "Content-Type": "application/json"
+  })
+
+  
 }
